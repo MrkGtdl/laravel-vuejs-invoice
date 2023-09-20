0a941c7b746b9a37df809f5ec9b12c842168dd59
MrkGtdl <markaustriagatdula@gmail.com>
2023-09-20 16:12:07 +0800

    update commit

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -10,7 +10,7 @@
         <link rel="preconnect" href="https://fonts.bunny.net">
         <link href="https://fonts.bunny.net/css?family=figtree:400,600&display=swap" rel="stylesheet" />
 
-        @vite('resouces/css/app.css')
+        @vite('resources/css/app.css')
     </head>
     <body class="antialiased">
         <div id="app">
