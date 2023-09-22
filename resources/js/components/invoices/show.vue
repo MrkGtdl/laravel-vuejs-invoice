d8cc367ee7ac191d13b8e7e5c103511ef5c90fa8
MrkGtdl <markaustriagatdula@gmail.com>
2023-09-23 00:06:13 +0800

    update commit

--- resources/js/components/invoices/show.vue
+++ resources/js/components/invoices/show.vue
@@ -17,7 +17,6 @@ onMounted(async () =>{
 
 const getInvoice = async () => {
     let response = await axios.get(`/api/show_invoice/${props.id}`)
-    // console.log('form',response.data.invoice)
     form.value = response.data.invoice
     console.log(form.value)
 }
